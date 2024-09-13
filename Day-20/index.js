3851c3dc48b31ef91b6f1eaa060ded24bd43f7a0
chukhibuon <vatbgct8b@gmail.com>
2024-09-14 00:48:38 +0700

    Sua bai tap

--- Day-20/index.js
+++ Day-20/index.js
@@ -1,25 +1,28 @@
 let text =
   "Lorem ipsum dolor sit, amet consectetur adipisicing elit magnam, praesentium excepturi voluptas fugit quasi porro itaque similique! Expedita pariatur eaque tempore impedit.";
-let wrappedText = text
-  .split(" ")
-  .map((word) => `<span>${word} </span>`)
-  .join("");
-document.write(wrappedText);
-let spans = document.querySelectorAll("span");
-let i = 0;
+let words = text.split(" ");
+let wrappedText = "";
 
-function highlightNextLetter() {
+for (let i = 0; i < words.length; i++) {
+  wrappedText += `<span> ${words[i]}</span>`;
+}
+document.write(wrappedText);
+
+let spans = document.querySelectorAll("span");
+
+let i = 0;
+function highlightWord() {
   if (i > 0) {
     spans[i - 1].classList.remove("highlight");
   }
   if (i < spans.length) {
     spans[i].classList.add("highlight");
     i++;
-    setTimeout(highlightNextLetter, 200);
+    setTimeout(highlightWord, 200);
   } else {
     i = 0;
-    setTimeout(highlightNextLetter, 200);
+    setTimeout(highlightWord, 200);
   }
 }
 
-highlightNextLetter();
+highlightWord();
